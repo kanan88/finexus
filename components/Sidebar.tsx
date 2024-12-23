65ad8aa23a25cd96cb4be4e181c5f8e00de2c6b1
Kanan Farzaliyev <kanan.farzali@gmail.com>
2024-12-23 17:21:56 +0100

    implement side bar for non-mobile size screens

---
+++ components/Sidebar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/icons/logo.svg";
+import { sidebarLinks } from "@/app/constants";
+import { cn } from "@/app/lib/utils";
+import { usePathname } from "next/navigation";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="flex mb-12 cursor-pointer items-center gap-2">
+          <Image
+            src={logo}
+            width={34}
+            height={34}
+            alt="Finexus Logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Finexus</h1>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}`);
+
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", {
+                "bg-bank-gradient": isActive,
+              })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p
+                className={cn("sidebar-label", {
+                  "!text-white": isActive,
+                })}
+              >
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
