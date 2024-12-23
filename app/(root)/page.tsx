1bef498e0acd2af21b10aa70a20a1cac71ca4fc7
Kanan Farzaliyev <kanan.farzali@gmail.com>
2024-12-23 03:39:34 +0100

    HeaderBox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,22 @@
+import HeaderBox from "@/components/HeaderBox";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const loggedIn = {
+    firstName: "Kanan",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account and transactions efficiently"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
